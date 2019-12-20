85220b03b251c78caab21becd93afeb4b4453e1b
Joe Henke <joed.henke@gamil.com>
2019-12-20 08:35:27 -0500

    Day 4 Part A

--- day4/day4.go
+++ day4/day4.go
@@ -7,5 +7,32 @@ func main() {
 }
 
 func PartA() int {
-	return  0
+	numMet := 0
+	for i := 402328; i <= 864247; i++ {
+		if meetCriteria(i) {
+			numMet++
+		}
+	}
+	return numMet
+}
+
+func meetCriteria(x int) bool {
+	digits := []int{
+		(x/100000) % 10,
+		(x/10000) % 10,
+		(x/1000) % 10,
+		(x/100) % 10,
+		(x/10) % 10,
+		(x/1) % 10,
+	}
+	hasDouble := false
+	for i := 0; i < len(digits)-1; i++ {
+		if digits[i] == digits[i+1] {
+			hasDouble = true
+		}
+		if digits[i] > digits[i+1] {
+			return false
+		}
+	}
+	return hasDouble
 }
---
+++ day4/day4_test.go
@@ -0,0 +1,24 @@
+package main
+
+import "testing"
+
+func TestMeetCriteria(t *testing.T) {
+	for _, tc := range []struct{
+		x int
+		pass bool
+	}{
+		{111111, true},
+		{223450, false},
+		{123789,  false},
+	}{
+		if got, want := meetCriteria(tc.x), tc.pass; got != want {
+			t.Errorf("%d got %v, want %v", tc.x, got, want)
+		}
+	}
+}
+
+func TestPartA(t *testing.T) {
+	if got, want := PartA(), 454; got != want {
+		t.Errorf("PartA  got %d, want %d", got, want)
+	}
+}
